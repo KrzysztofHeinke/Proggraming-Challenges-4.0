e11cb5b569756c3687e58b377796d9abcb4bcbc6
Krzysztof Heinke <Krzysztof.Heinke@gmail.com>
2020-08-18 19:17:17 +0200

    [Unique Ptr] - implementation of my unique ptr

---
+++ unique_ptr/source/unique_ptr.hpp
@@ -0,0 +1,69 @@
+#include <utility>
+
+template <class T>
+class my_unique_ptr
+{
+    private:
+    T* mPtr;
+    public:
+    T* get()
+    {
+        return mPtr;
+    }
+    void reset()
+    {
+        T *tmp = mPtr;
+        mPtr = nullptr;
+        delete tmp;
+    }
+    ~my_unique_ptr()
+    {
+        delete [] mPtr;
+    }
+    T operator*()
+    {
+        return *mPtr;
+    }
+    T* operator->()
+    {
+        return mPtr;
+    }
+    my_unique_ptr(T*&& ptr): mPtr(nullptr)
+    {
+        mPtr = std::move(ptr);
+    }
+    my_unique_ptr(): mPtr(nullptr)
+    {
+    }
+    my_unique_ptr operator=(T*&& ptr)
+    {
+        mPtr = std::move(ptr);
+    }
+    my_unique_ptr(my_unique_ptr<T>&& uniPtr): mPtr(nullptr)
+    {
+        mPtr = uniPtr.mPtr;
+        uniPtr.mPtr = nullptr;
+    }
+    my_unique_ptr operator=(my_unique_ptr<T>&& uniPtr)
+    {
+        mPtr = std::move(uniPtr.mPtr());
+    }
+
+
+    my_unique_ptr(const T ptr) = delete;
+    my_unique_ptr(const T* ptr) = delete;
+    my_unique_ptr(const T& ptr) = delete;
+    my_unique_ptr(const my_unique_ptr<T>& uniPtr) = delete;
+    my_unique_ptr operator=(const my_unique_ptr<T>& uniPtr) = delete;
+};
+
+template <class T>
+my_unique_ptr<T> make_unique(T*&& data)
+{
+    return my_unique_ptr<T>( std::move(data));
+}
+template <class T>
+my_unique_ptr<T> make_unique(T& data)
+{
+    return my_unique_ptr<T>( std::move(data));
+}
